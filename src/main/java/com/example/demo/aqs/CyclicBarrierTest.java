976ca6e9563d77c291edfc34f07cdcff3bf18188
huhui <415340216@qq.com>
2019-03-15 16:48:16 +0800

    CyclicBarrierTest

--- src/main/java/com/example/demo/aqs/CyclicBarrierTest.java
+++ src/main/java/com/example/demo/aqs/CyclicBarrierTest.java
@@ -12,31 +12,34 @@ import java.util.concurrent.Executors;
  */
 public class CyclicBarrierTest {
 
-    //定义屏障，指定数量为5个
-    private static CyclicBarrier barrier = new CyclicBarrier(5);
+    // 定义屏障，指定数量为5个
+   private final static CyclicBarrier barrier = new CyclicBarrier(5);
+
+   // 定义线程数量（余1个线程，会一直阻塞，这个时候就可以加时间了）
+   private final static int threadCount = 21;
 
     public static void main(String[] args) throws Exception {
 
         ExecutorService executor = Executors.newCachedThreadPool();
-        //往线程池中放入线程
-        for (int i = 0; i < 10; i++) {
-            final int threadNum = i;
+        for (int i = 0; i < threadCount; i++) {
+          final int threadNum = i;
             Thread.sleep(1000);
-            executor.execute(() -> {
-                try {
-                    race(threadNum);
-                } catch (Exception e) {
-                    e.printStackTrace();
+            executor.execute( () -> {
+                try{
+                    test(threadNum);
+                }catch(Exception e){
+                    System.out.println("异常");
                 }
             });
+
         }
         executor.shutdown();
     }
 
-    private static void race(int threadNum) throws Exception {
+    private static void test(int threadNum) throws Exception {
         Thread.sleep(1000);
         System.out.println(threadNum + " is ready");
-        //如果当前线程就绪，则告诉CyclicBarrier 需要等待
+        // 如果当前线程就绪，则告诉CyclicBarrier 需要等待（这是一个阻塞方法，等屏障一个一个减掉以后，又重新变成5个）
         barrier.await();
         // 当达到指定数量时，继续执行下面代码
         System.out.println(threadNum + " continue");

331fac62bb1ce0f281ee556fa122e1d1fe2af86e
huhui <415340216@qq.com>
2019-03-26 15:41:55 +0800

    fork and future add

--- src/main/java/com/example/demo/component/ForkJoinTest.java
+++ src/main/java/com/example/demo/component/ForkJoinTest.java
@@ -1,6 +1,7 @@
 package com.example.demo.component;
 
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.Future;
 import java.util.concurrent.RecursiveTask;
 
@@ -11,67 +12,74 @@ import java.util.concurrent.RecursiveTask;
  * @since 2019/1/17 15:13
  */
 public class ForkJoinTest extends RecursiveTask<Integer> {
-    //设置分割的阈值
+
+    // 设置分割的阀值
     public static final int threshold = 2;
+
     private int start;
+
     private int end;
+
+    private static int countFork = 0;
+    private static int countJoin = 0;
 
     public ForkJoinTest(int start, int end) {
         this.start = start;
         this.end = end;
     }
 
-    //任务
+    // 任务
+
     @Override
     protected Integer compute() {
         int sum = 0;
-
-        //如果任务足够小就计算任务
-        boolean canCompute = (end - start) <= threshold;
-        if (canCompute) {
-            //任务足够小的时候，直接计算，不进行分裂计算
-            for (int i = start; i <= end; i++) {
+        // 如果任务足够小就计算任务
+        boolean canCompute = (end - start) <+ threshold;
+        if(canCompute){
+            // 任务足够小的时候，直接计算，不进行分裂计算
+            for(int i = start; i <= end; i++){
                 sum += i;
             }
-        } else {
-            // 如果任务大于阈值，就分裂成两个子任务计算
-            int middle = (start + end) / 2;
-
-            /**
-             * 下面可能会产生递归操作
-             */
-            //继续分裂任务
+        }else{
+            // 如果任务大于阀值，就分裂成两个子任务计算
+            int middle = (start + end)/ 2;
+            // 分裂任务
             ForkJoinTest leftTask = new ForkJoinTest(start, middle);
             ForkJoinTest rightTask = new ForkJoinTest(middle + 1, end);
 
-            // 执行子任务
+            countFork ++;
+            System.out.println("countFork = " + countFork);
+            // 执行子任务；
             leftTask.fork();
             rightTask.fork();
 
-            // 等待任务执行结束合并其结果
+            // 等待任务执行结束获取结果
             int leftResult = leftTask.join();
             int rightResult = rightTask.join();
 
+            countJoin ++;
+            System.out.println("countJoin = " + countJoin);
+
             // 合并子任务
             sum = leftResult + rightResult;
+
         }
-        //返回结果
         return sum;
     }
 
     public static void main(String[] args) {
-        // 生成一个池
-        ForkJoinPool forkjoinPool = new ForkJoinPool();
+        // 生成一个池；
+        ForkJoinPool pool = new ForkJoinPool();
 
-        // 生成一个计算任务，计算1+2+3+4
-        ForkJoinTest task = new ForkJoinTest(1, 100);
+        // 生成一个计算任务
+        ForkJoinTask task = new ForkJoinTest(1, 100);
 
         // 执行一个任务，将任务放入池中，并开始执行，并用Future接收
-        Future<Integer> result = forkjoinPool.submit(task);
+        Future<Integer> result = pool.submit(task);
 
-        try {
+        try{
             System.out.println("result = " + result.get());
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
--- src/main/java/com/example/demo/component/FutureTaskTest.java
+++ src/main/java/com/example/demo/component/FutureTaskTest.java
@@ -1,7 +1,6 @@
 package com.example.demo.component;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 /**
  * TODO
@@ -11,31 +10,47 @@ import java.util.concurrent.FutureTask;
  */
 public class FutureTaskTest {
 
-    public static void main(String[] args) throws Exception {
+    private final static ExecutorService executorService = Executors.newCachedThreadPool();
 
-        FutureTask<String> futureTask = new FutureTask<String>(new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                System.out.println("do something in callable");
-                Thread.sleep(5000);
-                return "Done";
-            }
-        });
-
-        new Thread(futureTask).start();
-        System.out.println("do something in main");
-        // 1. 调用isDone()判断任务是否结束
-        if(!futureTask.isDone()) {
-            System.out.println("Task is not done");
-            try {
-                //阻塞主线程一秒钟
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    static class MyCallable implements Callable<String>{
+        @Override
+        public String call() throws Exception {
+            System.out.println("do something in callable");
+            Thread.sleep(5000);
+            return "Done";
         }
-        String result = futureTask.get();
+    }
+
+    public static void main(String[] args) throws Exception {
+        // Future测试
+        futureTest();
+        // futureTask测试
+        futureTaskTest();
+    }
+
+    public static void futureTest() throws Exception{
+        // 线程池提交Callable任务，并得到Future
+        Future<String> future = executorService.submit(new MyCallable());
+        System.out.println("do something in main");
+        Thread.sleep(1000);
+        String result = future.get();
         System.out.println("result = " + result);
     }
 
+    public static void futureTaskTest() throws ExecutionException, InterruptedException {
+        FutureTask<String> futureTask = new FutureTask<String>(()-> {
+            System.out.println("do something in callable");
+            Thread.sleep(5000);
+            return "Done";
+        });
+        executorService.submit(futureTask);
+        System.out.println("do something in main");
+        // 调用isDone()判断任务是否结束
+        if(!futureTask.isDone()){
+            System.out.println("task is not done");
+        }
+        // get()是个阻塞方法
+        String result = futureTask.get();
+        System.out.println("result = " + result);
+    }
 }
